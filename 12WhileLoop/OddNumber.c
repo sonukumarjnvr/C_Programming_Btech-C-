318d6e2d65ccc998aa004563ce2e2867177d24c1
sonukumarjnvr <sonukumarjnvr@gmail.com>
2024-09-28 15:38:46 +0530

    while loop

---
+++ 12WhileLoop/OddNumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int N;
+    printf("enter a number to print odd numbers : ");
+    scanf("%d",&N);
+
+    int i=1;
+    while(i<=N){
+        if(i % 2 != 0){
+            printf("%d ",i);
+        }
+        i++;
+    }
+    return 0;
+}
